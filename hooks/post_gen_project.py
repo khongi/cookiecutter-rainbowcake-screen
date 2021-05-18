e01b304cf8d5acb3c3da577990ff7ee2824b490a
Khongor Bayarsaikhan <b.khongi@gmail.com>
2021-05-18 12:50:53 +0200

    Extract directory search into function

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -27,40 +27,48 @@ def split_on_uppercase(s, keep_contiguous=False):
 
     return parts
 
-start_path = os.getcwd()
+def find_node(start_path, searching_for, is_file=False):
+    last_root = start_path
+    current_root = start_path
 
-last_root = start_path
-current_root = start_path
-res_dir = None
-searching_for = 'res'
+    while current_root:
+        pruned = False
+        for root, dirs, files in os.walk(current_root):
+            if not pruned:
+                try:
+                    # Remove the part of the tree we already searched
+                    del dirs[dirs.index(os.path.basename(last_root))]
+                    pruned = True
+                except ValueError:
+                    pass
+            if (is_file):
+                if searching_for in files:
+                    # Found the searched file
+                    return os.path.join(root, searching_for)
+            else:
+                if searching_for in dirs:
+                    # Found the searched dir
+                    return os.path.join(root, searching_for)
+                
+        # Otherwise, pop up a level, search again
+        last_root = current_root
+        current_root = os.path.dirname(last_root)
 
-while res_dir is None and current_root:
-    pruned = False
-    for root, dirs, files in os.walk(current_root):
-        if not pruned:
-            try:
-                # Remove the part of the tree we already searched
-                del dirs[dirs.index(os.path.basename(last_root))]
-                pruned = True
-            except ValueError:
-                pass
-        if searching_for in dirs:
-            # found the dir, stop
-            res_dir = os.path.join(root, searching_for)
-            print("Will create layout file in: " + res_dir)
-            break
-    # Otherwise, pop up a level, search again
-    last_root = current_root
-    current_root = os.path.dirname(last_root)
+current_dir = os.getcwd()
 
+# Find resources directory
+res_dir = find_node(os.getcwd(), 'res')
+
+# Get generated layout file
 generated_layout_name = "$LAYOUTFILE.xml"
-generated_layout_file = os.path.join(start_path, generated_layout_name)
+generated_layout_file = os.path.join(current_dir, generated_layout_name)
 
+# Make sure layout directory exists
 layout_dir = os.path.join(res_dir, "layout/")
 Path(layout_dir).mkdir(parents=True, exist_ok=True)
 
+# Move generated layout file to layout directory with proper name
 screen_name_splitted = '_'.join(split_on_uppercase("{{ cookiecutter.screen_name }}")).lower()
 new_layout_file_name = 'fragment_' + screen_name_splitted + '.xml'
 new_layout_file = os.path.join(layout_dir, new_layout_file_name)
-
 os.rename(generated_layout_file, new_layout_file)
