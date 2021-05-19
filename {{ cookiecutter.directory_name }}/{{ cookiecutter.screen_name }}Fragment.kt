13b55771994ec4d91aa87580a2649a880c337a5e
Khongor Bayarsaikhan <b.khongi@gmail.com>
2021-05-19 12:25:03 +0200

    Use rainbowcake dagger module if Dagger 2 is chosen

--- {{ cookiecutter.directory_name }}/{{ cookiecutter.screen_name }}Fragment.kt
+++ {{ cookiecutter.directory_name }}/{{ cookiecutter.screen_name }}Fragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+{%- if cookiecutter.di == "Hilt" %}
 import androidx.fragment.app.viewModels
+{%- endif %}
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
+{%- if cookiecutter.di == "Dagger 2" %}
+import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
+{%- endif %}
 import ${app_package}.databinding.Fragment{{ cookiecutter.screen_name }}Binding
 {%- if cookiecutter.di == "Hilt" %}
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,8 +21,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class {{ cookiecutter.screen_name }}Fragment : RainbowCakeFragment<{{ cookiecutter.screen_name }}ViewState, {{ cookiecutter.screen_name }}ViewModel>() {
 
     private lateinit var binding: Fragment{{ cookiecutter.screen_name }}Binding
-
+    {% if cookiecutter.di == "Hilt" %}
     override fun provideViewModel() = viewModels<{{ cookiecutter.screen_name }}ViewModel>().value
+    {%- elif cookiecutter.di == "Dagger 2" %}
+    override fun provideViewModel() = getViewModelFromFactory()
+    {%- endif %}
 
     override fun onCreateView(
         inflater: LayoutInflater,
